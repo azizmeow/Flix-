cce1fe348677e2056538f682d49015d1117eb103
azizmeow <azizsalim02@gmail.com>
2021-10-21 18:04:30 +0530

    This is the first commit after doing some of the work in the project. Client side coding is remaining. Also, need to modify HTML and CSS as per our design. Node server has been created in this commit.

---
+++ Node Server/index.js
@@ -0,0 +1,18 @@
+const io = require('socket.io')(8000);
+
+const users = {}
+
+io.on('connection', (socket)=>{
+    socket.on('new-user-joined', (name)=>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    });
+
+    socket.on('send', (message)=>{
+        socket.broadcast.emit('recieve', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', (name)=>{
+        socket.broadcast.emit('left', name);
+    });
+});
